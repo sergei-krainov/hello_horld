212bd5d85fc5f3f818de50589b2530ba33a9677e
Sergei Krainov <sergei.krainov.mns@gmail.com>
2016-09-09 21:04:53 +0300

    hello.c+Makefile. Cross compile is not yet ready

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+
+static int hello_horld_init(void) {
+    printk(KERN_INFO "Hello horld\n");
+    return 0;
+}
+
+static void hello_horld_exit(void) {
+    printk(KERN_INFO "Goodbye horld\n");
+}
+
+module_init(hello_horld_init);
+module_exit(hello_horld_exit);
